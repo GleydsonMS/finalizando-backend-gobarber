264062c2d3c776b0760d1b44c42c3d3db24c4019
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-05-12 23:15:44 -0300

    Injeção de dependências

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
+import { container } from 'tsyringe';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import AuthenticateUserServer from '@modules/users/services/AuthenticateUserService';
 
 const sessionsRouter = Router();
@@ -8,8 +8,7 @@ const sessionsRouter = Router();
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
 
-  const usersRepository = new UsersRepository();
-  const authenticateUser = new AuthenticateUserServer(usersRepository);
+  const authenticateUser = container.resolve(AuthenticateUserServer);
 
   const { user, token } = await authenticateUser.execute({
     email,
