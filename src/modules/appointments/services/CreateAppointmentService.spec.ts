4fc322d2969dffba8602c3e809b6805b131e0471
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-05-13 22:12:42 -0300

    Criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    // to do
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const CreateAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await CreateAppointment.execute({
+      date: new Date(),
+      provider_id: '123456789',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456789');
   });
 
   // it('should not be able to create two appointments on the same time', () => {
