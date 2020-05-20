5e6679d0ba93645985b23eda8a62c0a564e3bab0
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-05-19 23:32:02 -0300

    Refatoração dos testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,14 +2,18 @@ import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointment: CreateAppointmentService;
+
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const CreateAppointment = new CreateAppointmentService(
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
     );
-
-    const appointment = await CreateAppointment.execute({
+  });
+  it('should be able to create a new appointment', async () => {
+    const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '123456789',
     });
@@ -19,19 +23,14 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const CreateAppointment = new CreateAppointmentService(
-      fakeAppointmentsRepository,
-    );
-
     const appointmentDate = new Date(2020, 4, 10, 11);
 
-    await CreateAppointment.execute({
+    await createAppointment.execute({
       date: appointmentDate,
       provider_id: '123456789',
     });
 
-    await expect(CreateAppointment.execute({
+    await expect(createAppointment.execute({
       date: appointmentDate,
       provider_id: '123456789',
     })).rejects.toBeInstanceOf(AppError);
